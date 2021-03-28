ababef0de5f2846eac32ce31043c29e4811ca428
Stephanie <stephanie.enciso13@gmail.com>
2021-03-28 14:25:56 -0500

    The search endpoint is working

--- api/users/renters-router.js
+++ api/users/renters-router.js
@@ -15,18 +15,38 @@ router.get('/:id', validateItemId, (req, res) => {
     res.status(200).json(res.item)
 })
 
+// router.get('/', (req, res) => {
+//     Renter.findAllItems()
+//       .then(items => {
+//           res.status(200).json(items)
+//       })
+//       .catch(err => {
+//           res.status(500).json({
+//               message: `Server error: ${err.message}`
+//           })
+//       })
+// })
+
 router.get('/', (req, res) => {
-    Renter.findAllItems()
-      .then(items => {
-          res.status(200).json(items)
-      })
-      .catch(err => {
-          res.status(500).json({
-              message: `Server error: ${err.message}`
+    let filter = req.query.filter
+    let location = req.query.location
+    if(!filter && !location) {
+        res.status(400).json({
+            message: 'Please provide either a name and/or location to search.'
+        })
+    } else {
+        filter = '%'+filter+'%'
+        location = '%'+location+'%'
+        Renter.searchItem(filter, location)
+          .then(items => {
+              res.status(200).json(items)
           })
-      })
+          .catch(err => {
+              res.status(500).json({
+                  message: `Server error: ${err.message}`
+              })
+          })
+    }
 })
-
-
 
 module.exports = router
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -30,16 +30,15 @@ function findAllItems() {
 }
 
 function searchItem(filter, location) {
-    return db('items as i')
-        .join('users as u')
+    return db('items as i').join('users as u', 'i.user_id', 'u.user_id')
         .select('i.item_id as item_id', 'i.name as name', 'i.category as category',
          'i.price_per_day as price_per_day', 'i.rental_period as rental_period',
-         'i.description as description', 'u.username as ownerUsername', 'u.email as ownerEmail', 
-         'u.firstname as ownerFirstname', 'u.lastname as ownerLastname', 'u.streetAddress as streetAddress', 
+         'i.description as description', 'u.username as username', 'u.email as email', 
+         'u.firstname as firstname', 'u.lastname as lastname', 'u.streetAddress as streetAddress', 
          'u.city as city', 'u.state as state', 'u.zipcode as zipcode', 'i.user_id as user_id')
-        .where('name', 'like', filter)
-        .orWhere('category', 'like', filter)
-        .andWhere('city', 'like', location)
-        .orWhere('state', 'like', location)
-        .orWhere('zipcode', 'like', location)
+        .where('i.name', 'like', filter)
+        .orWhere('i.category', 'like', filter)
+        .andWhere('u.city', 'like', location)
+        .orWhere('u.state', 'like', location)
+        .orWhere('u.zipcode', 'like', location)
 }
