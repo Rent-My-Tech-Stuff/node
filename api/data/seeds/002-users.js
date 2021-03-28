8127f7859b7fe038fd7fe4860e1e8acbf90ead93
Stephanie <stephanie.enciso13@gmail.com>
2021-03-28 14:47:15 -0500

    heroku push

--- api/data/seeds/002-users.js
+++ api/data/seeds/002-users.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   return knex('users').insert([
-    {username: 'test', firstname: 'testing', lastname: 'tests', email: 'test@testing.com', password: 'test123', streetAddress: '1548 Test St.', city: 'Dallas', state: 'Texas', zipcode: '74802', role: 'owner'},
+    {username: 'test1', firstname: 'testing', lastname: 'tests', email: 'test1@testing.com', password: 'test123', streetAddress: '1548 Test St.', city: 'Dallas', state: 'Texas', zipcode: '74802', role: 'owner'},
     {username: 'Steph', firstname: 'Steph', lastname: 'Anie', email: 'me@me.com', password: 'test123', streetAddress: '12345 Abc St.', city: 'Owasso', state: 'Oklahoma', zipcode: '74055', role: 'renter'},
     {username: 'testing1', firstname: 'test1', lastname: 'test1', email: 'testing1@test1.com', password: 'test12345', streetAddress: '15896 Weird Ave.', city: 'Los Angeles', state: 'California', zipcode: '89456', role: 'owner'}
   ]);
